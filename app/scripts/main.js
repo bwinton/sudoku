9898e762a527d3d74325391ab3ba371f35222655
Blake Winton <bwinton@latte.ca>
2014-01-01 18:34:36 -0500

    Get the edge detection working!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,8 @@
 
 $(function () {
 
-  var getColorAtOffset = function (data, offset) {
+  var getColor = function (data, x, y, width) {
+    var offset = (y * width + x) * 4;
     return {
       red: data[offset],
       green: data[offset + 1],
@@ -18,49 +19,68 @@ $(function () {
     };
   };
 
-  var setColorAtOffset = function (data, offset, color) {
+  var setColor = function (data, x, y, width, color) {
+    var offset = (y * width + x) * 4;
     data[offset] = color.red;
     data[offset + 1] = color.green;
     data[offset + 2] = color.blue;
     data[offset + 3] = color.alpha;
   };
 
-  var onFrame = function (canvas) {
-    var contrastFactor = 1.0;
+  var calcBrightness = function (color) {
+    // calculate pixel brightness
+    // http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+    var brightness = (0.299 * color.red + 0.587 * color.green + 0.114 * color.blue);
+    return brightness;
+  }
 
+  var kernel = [
+    [3,   3, 3],
+    [3, -27, 3],
+    [3,   3, 3]
+  ];
+
+  var onFrame = function (canvas) {
     var context = canvas.getContext('2d');
     var canvasWidth = canvas.width;
     var canvasHeight = canvas.height;
-    console.log(canvasWidth, canvasHeight);
     var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
 
     var outContext = $('#tempResult')[0].getContext('2d');
+    var outData = outContext.getImageData(0, 0, canvasWidth, canvasHeight);
 
 
     for (var y = 0; y < canvasHeight; y++) {
       for (var x = 0; x < canvasWidth; x++) {
         // get each pixel's brightness
 
-        var offset = (y * canvasWidth + x) * 4;
-
-        var color = getColorAtOffset(imageData.data, offset);
+        var color = getColor(imageData.data, x, y, canvasWidth);
+        var brightness = calcBrightness(color);
   
-        // calculate pixel brightness
-        // http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
-        var brightness = (0.299 * color.red + 0.587 * color.green + 0.114 * color.blue) / 255;
-        brightness = Math.round(brightness * contrastFactor);
-        brightness = brightness * 255;
-        // Grey
-        var greyColor = {
-          red: brightness,
-          green: brightness,
-          blue: brightness,
+        var sum = 0; // Kernel sum for this pixel
+        for (var ky = -1; ky <= 1; ky++) {
+          for (var kx = -1; kx <= 1; kx++) {
+            // Calculate the adjacent pixel for this kernel point
+            var adjacentColor = getColor(imageData.data, x + kx, y + ky, canvasWidth);
+            var val = calcBrightness(adjacentColor);
+            // Multiply adjacent pixels based on the kernel values
+            sum += kernel[ky+1][kx+1] * val;
+          }
+        }
+
+        sum = Math.round(sum / 255) * 255;
+
+        var outColor = {
+          red: color.red,
+          green: Math.max(color.green, sum),
+          blue: color.blue,
           alpha: color.alpha
         };
-        setColorAtOffset(imageData.data, offset, greyColor);
+
+        setColor(outData.data, x, y, canvasWidth, outColor);
       }
     }
-    outContext.putImageData(imageData, 0, 0);
+    outContext.putImageData(outData, 0, 0);
   };
 
   camera.init({
