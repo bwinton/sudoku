8ea917473c71f0652dd1f23f516471bf0cb0838d
Blake Winton <bwinton@latte.ca>
2014-01-01 20:24:41 -0500

    Split out the edge stuff, and add some hough comments.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,37 +9,6 @@
 
 $(function () {
 
-  var getColor = function (data, x, y, width) {
-    var offset = (y * width + x) * 4;
-    return {
-      red: data[offset],
-      green: data[offset + 1],
-      blue: data[offset + 2],
-      alpha: data[offset + 3]
-    };
-  };
-
-  var setColor = function (data, x, y, width, color) {
-    var offset = (y * width + x) * 4;
-    data[offset] = color.red;
-    data[offset + 1] = color.green;
-    data[offset + 2] = color.blue;
-    data[offset + 3] = color.alpha;
-  };
-
-  var calcBrightness = function (color) {
-    // calculate pixel brightness
-    // http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
-    var brightness = (0.299 * color.red + 0.587 * color.green + 0.114 * color.blue);
-    return brightness;
-  }
-
-  var kernel = [
-    [3,   3, 3],
-    [3, -27, 3],
-    [3,   3, 3]
-  ];
-
   var onFrame = function (canvas) {
     var context = canvas.getContext('2d');
     var canvasWidth = canvas.width;
@@ -49,38 +18,21 @@ $(function () {
     var outContext = $('#tempResult')[0].getContext('2d');
     var outData = outContext.getImageData(0, 0, canvasWidth, canvasHeight);
 
+    detectEdges(imageData, outData, canvasWidth, canvasHeight);
 
-    for (var y = 0; y < canvasHeight; y++) {
-      for (var x = 0; x < canvasWidth; x++) {
-        // get each pixel's brightness
-
-        var color = getColor(imageData.data, x, y, canvasWidth);
-        var brightness = calcBrightness(color);
-  
-        var sum = 0; // Kernel sum for this pixel
-        for (var ky = -1; ky <= 1; ky++) {
-          for (var kx = -1; kx <= 1; kx++) {
-            // Calculate the adjacent pixel for this kernel point
-            var adjacentColor = getColor(imageData.data, x + kx, y + ky, canvasWidth);
-            var val = calcBrightness(adjacentColor);
-            // Multiply adjacent pixels based on the kernel values
-            sum += kernel[ky+1][kx+1] * val;
-          }
-        }
-
-        sum = Math.round(sum / 255) * 255;
-
-        var outColor = {
-          red: color.red,
-          green: Math.max(color.green, sum),
-          blue: color.blue,
-          alpha: color.alpha
-        };
-
-        setColor(outData.data, x, y, canvasWidth, outColor);
-      }
-    }
     outContext.putImageData(outData, 0, 0);
+
+    // At this point, I've got the edges, so I should:
+    // 1) run the Hough Transform,
+    //   a) Create an accumulator array of π by sqrt(width^2 + height^2).
+    //   b) For each edge point,
+    //   c)   for each Θ between 0 and π,
+    //   d)     Calculate r = x*cos(Θ) + y*sin(Θ)
+    //   e)     Add one to the accumulator at Θ,r.
+    // 2) get the four brightest lines (four highest-valued points in the accumulator)
+    // 3) find their intersections, and
+    // 4) plot the four lines between their intersections!  :)
+
   };
 
   camera.init({
